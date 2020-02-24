7da7945b07ff7c5d9f6339c0aea3808d3bbf258e
nmahlberg15@gmail.com <38565670+nmahlberg@users.noreply.github.com>
2020-02-23 23:36:23 -0600

    I don't know what's wrong

---
+++ :script.js
@@ -0,0 +1,19 @@
+﻿function calc(){
+    let tip = document.getElementById("percent").value;
+    let amount = document.getElementById("amount").value;
+    if (tip == "") {
+        alert("Enter something for tip please");
+    }
+    if (amount == "") {
+        alert("Enter something for amount please");
+    }
+    if (tip > 1) {
+        tip = tip / 100;
+    }
+    alert(tip * amount);
+    document.getElementById("tip1").innerHTML = "$" + tip * amount;
+    document.getElementById("tax").innerHTML = "$" + amount * .06225;
+    document.getElementById("total").innerHTML = "$" + amount + (amount * .06225) + (amount * tip);
+    return false;
+
+}
